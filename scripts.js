d3c2084adb9f499aac05b0c99b3cf59d4c003945
Ngethe <ciru930@gmail.com>
2019-12-02 11:21:54 +0300

    add alert function to get result on script.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,23 @@
-var checkDayOfWeek=fuction(){
-   
-    var male = ['Kwasi', 'Kwandwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-    var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+function akanNames() {
+  var day = parseInt(document.getElementById("d").value);
+  var month = parseInt(document.getElementById("m").value);
+  var year = parseInt(document.getElementById("y").value);
 
-    var date = document.getElementById("d").value;
-    var month = document.getElementById("m").value;
-    var year = document.getElementById("yy").value;
-    var male = document.getElementById("male");
-    var female = document.getElementById("female")
-    var currentDate = new Date(year + "/" + month + "/" + date);
-    var currentYear = currentDate.getDay();
 
-    if (male.checked) {
-        document.getElementById("x ").innerHTML = " your Akan name is " + maleNames[currentYear] + " so you were born on " + daysOfTheWeek[currentYear]
-      } else if (female.checked) {
-        document.getElementById(" x").innerHTML = "your Akan name is " + femaleNames[currentYear] + " so you were born on " + daysOfTheWeek[currentYear]
-      }
-    
-    
-    $ (document).ready(function(){
-      $
-    })
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
+  var display = document.getElementById("display");
 
+
+  var dateOfTheWeek = new Date(year + "/" + month + "/" + day);
+  var result = dateOfTheWeek.getDay();
+  alert(result);
+
+
+
+  var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var maleNames = ["Kwasi", " Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  alert(maleNames[0]);
+}

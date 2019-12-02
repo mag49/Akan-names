a0e6d96290c1da38ed6d968df86cf32811a1299b
Ngethe <ciru930@gmail.com>
2019-12-02 03:01:06 +0300

    add female string to script.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 var checkDayOfWeek=fuction(){
    
     var male = ['Kwasi', 'Kwandwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 }

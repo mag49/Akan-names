fabc959af15e45428abca00f925a23fa787be10e
Ngethe <ciru930@gmail.com>
2019-12-02 04:11:26 +0300

    add the function for calculating the weekday

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
 var checkDayOfWeek=fuction(){
    
-    var male = ['Kwasi', 'Kwandwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-    var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-    var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    var male = ['Kwasi', 'Kwandwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     var date = document.getElementById("d").value;
     var month = document.getElementById("m").value;
-    var month = document.getElementById("m").value;
-}
+    var year = document.getElementById("yy").value;
+    var male = document.getElementById("male");
+    var female = document.getElementById("female")
+    var currentDate = new Date(year + "/" + month + "/" + date);
+    var currentYear = currentDate.getDay();
+
+    if (male.checked) {
+        document.getElementById("x ").innerHTML = " your Akan name is " + maleNames[currentYear] + " so you were born on " + daysOfTheWeek[currentYear]
+      } else if (female.checked) {
+        document.getElementById(" x").innerHTML = "your Akan name is " + femaleNames[currentYear] + " so you were born on " + daysOfTheWeek[currentYear]
+      }
+    
+    
+    $ (document).ready(function(){
+      $
+    })
+

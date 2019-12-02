38fd04621189d7908da8744d8fa6aa9254fac2b3
Ngethe <ciru930@gmail.com>
2019-12-02 03:06:49 +0300

    add string of day of the week

--- scripts.js
+++ scripts.js
@@ -2,4 +2,5 @@ var checkDayOfWeek=fuction(){
    
     var male = ['Kwasi', 'Kwandwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
     var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+    var dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 }
